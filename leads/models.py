5d91a5219ff41fc5ee8648a7898de3a06602ba54
Samuel <samuelnunezdev@gmail.com>
2022-02-28 13:46:59 -0400

    untrack some files

--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,41 @@
-from django.db import models
-
 # We represent data structure of our web page in models.py
 
-class Lead(models.Model): 
-    first_name = models.CharField(max_length=20) # we created a field
-    # CharField() restricts the filed to be character in a database
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    #SOURCE_CHOICES = (
+    #    ('YouTube', 'YouTube'), # database value, display value
+    #    ('Google', 'Google'),
+    #    ('Newsletter', 'Newsletter'),
+    #)
+    first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE) 
+        # we link a table to another table database
+        # we link agent variable to agent model
+        # ForeignKeys need 'on_delete' so we know what will hapen if
+        # we delete that Agent
+        # .CASCADE      if the Agent is deleted, delete the lead
+        # .SET_NULL, null=True
+        # .SET_DEFAULT, null=True, default=        
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # don't need these 2, we already have them in AbstractUser
+    # first_name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
+
+
+# =================== NOTES ====================
+
+    #phoned = models.BooleanField(default=False)
+    #source = models.CharField(choices = SOURCE_CHOICES, max_length=100)
+
+    #profile_picture = models.ImageField(blank=True, null=True) # submitting a empty value, no value in database
+    #special_files = models.FileField(blank=True, null=True)
